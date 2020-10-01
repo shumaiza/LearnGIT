5165652ed51610c52eff806f2bb2be27a891b4a0
shumaiza <shammu1@gmail.com>
2020-10-01 20:17:32 +0100

    source files

---
+++ src/main/java/learn/git/AppTest.java
@@ -0,0 +1,10 @@
+package learn.git;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+ @Test
+    public void Access(){
+        System.out.println("Access the application");
+    }
+}
---
+++ src/main/java/learn/git/LoginTest.java
@@ -0,0 +1,11 @@
+package learn.git;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+  @Test
+    void Login(){
+        System.out.println("Enter the username and password");
+    }
+
+}
